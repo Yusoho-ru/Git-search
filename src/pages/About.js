37ec1d3184628ac3a8b478f9d4ae278f11926447
Yusoho-ru <kapitanov2703@gmail.com>
2021-07-26 15:18:16 +0300

    First commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const About = () => {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Инфа</h1>
+                <p class="lead">Версия: <strong>1.0.0</strong></p>
+            </div>
+        </div>
+    )
+}
